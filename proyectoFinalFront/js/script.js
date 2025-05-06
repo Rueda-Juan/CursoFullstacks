334c7261eb01a4e21ec398bc07b71b73a0168183
javiRodrigue <agudtinjava@gmail.com>
2025-05-06 01:57:37 -0300

    agregando script de busqueda y validaciones del buscador de productos

--- proyectoFinalFront/js/script.js
+++ proyectoFinalFront/js/script.js
@@ -25,3 +25,47 @@ function validarForm(event) {
   }
 
 }
+
+
+//Script de buscador de productos
+function normalizar(text){
+  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(); 
+}
+
+document.getElementById('buscadorProducto').addEventListener('submit',function(e){
+  e.preventDefault();
+  //Diccionario de productos
+  //Clave: nombre buscado , valor: archivo HTML
+  const productos ={
+    "celular" : "/CursoFullstacks/proyectoFinalFront/html/productos/TecnologiaCelular.html",
+    "heladera" : "/CursoFullstacks/proyectoFinalFront/html/productos/TecnologiaHeladera.html",
+    "horno" : "/CursoFullstacks/proyectoFinalFront/html/productos/TecnologiaHorno.html",
+    "lavarropas" : "/CursoFullstacks/proyectoFinalFront/html/productos/TecnologiaLavarropas.html",
+    "notebook" : "/CursoFullstacks/proyectoFinalFront/html/productos/TecnologiaNotebook.html",
+    "tablet" : "/CursoFullstacks/proyectoFinalFront/html/productos/TecnologiaTablet.html",
+  };
+
+  //Obtengo la busqueda del usuario
+  const busqueda = document.getElementById('inputProducto').value.trim();
+
+  const normalizada = normalizar(busqueda);
+
+
+  //busqueda secuencial, encontrar primer coincidencia exacta
+  for(let clave in productos){
+    if(normalizar(clave) === normalizada){
+      window.location.href = productos[clave];
+      return;
+    }
+  }
+
+  //busqueda secuencial, encontrar coincidencia parcial (primer match en letras)
+  for(let clave in productos){
+    if(normalizar(clave).includes(normalizada)){
+      window.location.href = productos[clave];
+      return;
+    }
+  }
+
+  alert("Producto no encontrado");
+})
